2ee89aa295e5571e0ddaa566e40e22ee205369c8
Jared Stoker <stokerfam@gmail.com>
2014-07-10 20:27:23 -0600

    Basic Menus added

--- SchoolDatabase/main.cpp
+++ SchoolDatabase/main.cpp
@@ -1,10 +1,207 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
+void studentMenu();
+void adminMenu();
+void defaultMenu();
+void teacherMenu()
+{
+    {
+        char selection;
 
+           do
+           {//menu
+               cout << "  Teacher Menu - Choose your role:\n";
+               cout << "  ====================================\n";
+               cout << "  1.  Student\n";
+               cout << "  2.  Teacher\n";
+               cout << "  3.  Admin\n";
+               cout << "  4.  Back to Main Menu\n";
+               cout << "\n";
+               cout << "  5.  Exit\n";
+               cout << "  ====================================\n";
+               cout << "  Enter your selection: ";
+               cin >> selection;
+               cout << endl;
+
+               switch (selection)
+               {
+                   case '1':
+                       //Student
+                       studentMenu();
+                       break;
+
+                   case '2':
+                       //Teacher
+                       teacherMenu();
+                       break;
+                   case '3':
+                       //Admin
+                       adminMenu();
+                       break;
+                   case '4':
+                       defaultMenu();
+                       break;
+                   case '5':
+                       cout << "Goodbye.\n";
+                       selection = 0;
+                       break;
+
+                   default: cout <<selection << "is not a valid menu item.\n";
+                   cout << endl;
+               }
+           }while (selection != 0 );
+    }
+}
+
+void studentMenu()
+{
+    {
+        char selection;
+
+           do
+           {//menu
+               cout << "  Student Menu:\n";
+               cout << "  ====================================\n";
+               cout << "  1.  Student\n";
+               cout << "  2.  Teacher\n";
+               cout << "  3.  Admin\n";
+               cout << "  4.  Back to Main Menu\n";
+               cout << "\n";
+               cout << "  5.  Exit\n";
+               cout << "  ====================================\n";
+               cout << "  Enter your selection: ";
+               cin >> selection;
+               cout << endl;
+
+               switch (selection)
+               {
+                   case '1':
+                       //Student
+                       studentMenu();
+                       break;
+
+                   case '2':
+                       //Teacher
+                       teacherMenu();
+                       break;
+                   case '3':
+                       //Admin
+                       adminMenu();
+                       break;
+                   case '4':
+                       defaultMenu();
+                       break;
+                   case '5':
+                       cout << "Goodbye.\n";
+                       selection = 0;
+                       break;
+
+                   default: cout <<selection << "is not a valid menu item.\n";
+                   cout << endl;
+               }
+           }while (selection != 0 );
+    }
+}
+
+void adminMenu()
+{
+    {
+        char selection;
+
+           do
+           {//menu
+               cout << "  Admin Menu:\n";
+               cout << "  ====================================\n";
+               cout << "  1.  Student\n";
+               cout << "  2.  Teacher\n";
+               cout << "  3.  Admin\n";
+               cout << "  4.  Back to Main Menu\n";
+               cout << "\n";
+               cout << "  5.  Exit\n";
+               cout << "  ====================================\n";
+               cout << "  Enter your selection: ";
+               cin >> selection;
+               cout << endl;
+
+               switch (selection)
+               {
+                   case '1':
+                       //Student
+                       studentMenu();
+                       break;
+
+                   case '2':
+                       //Teacher
+                       teacherMenu();
+                       break;
+                   case '3':
+                       //Admin
+                       adminMenu();
+                       break;
+                   case '4':
+                       defaultMenu();
+                       break;
+                   case '5':
+                       cout << "Goodbye.\n";
+                       selection = 0;
+                       break;
+
+                   default: cout <<selection << "is not a valid menu item.\n";
+                   cout << endl;
+               }
+           }while (selection != 0 );
+    }
+}
+
+void defaultMenu()
+{
+    {
+        char selection;
+
+           do
+           {//menu
+               cout << "  Main Menu - Choose your role:\n";
+               cout << "  ====================================\n";
+               cout << "  1.  Student.\n";
+               cout << "  2.  Teacher\n";
+               cout << "  3.  Admin\n";
+               cout << "\n";
+               cout << "  4.  Exit\n";
+               cout << "  ====================================\n";
+               cout << "  Enter your selection: ";
+               cin >> selection;
+               cout << endl;
+
+               switch (selection)
+               {
+                   case '1':
+                       //Student
+                       studentMenu();
+                       break;
+
+                   case '2':
+                       //Teacher
+                       teacherMenu();
+                       break;
+                   case '3':
+                       //Admin
+                       adminMenu();
+                       break;
+                   case '4':
+                       cout << "Goodbye.\n";
+                       selection = 0;
+                       break;
+
+                   default: cout <<selection << "is not a valid menu item.\n";
+                   cout << endl;
+               }
+           }while (selection != 0 );
+    }
+}
 int main()
 {
-    cout << "Hello World!" << endl;
-    return 0;
+   defaultMenu();
 }
 
